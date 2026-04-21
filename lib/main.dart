00203027ae77a0a08e63c2f8a3bb107348405c75
Ellaine Joyce Barro <mpacc006@gmail.com>
2026-04-21 13:58:59 +0800

    UPDATE: fonts added, theme update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'constants/colors.dart' as clr;
 
 void main() {
-  runApp(MaterialApp(home: const RecordsPage()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        navigationBarTheme: NavigationBarThemeData(
+          indicatorColor: Colors.transparent,
+          indicatorShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero,
+          ),
+          iconTheme: WidgetStateProperty.resolveWith((
+            states,
+          ) {
+            if (states.contains(WidgetState.selected)) {
+              return IconThemeData(color: clr.greyBlue);
+            }
+
+            return IconThemeData(color: Colors.white);
+          }),
+          labelTextStyle: WidgetStateProperty.resolveWith((
+            states,
+          ) {
+            if (states.contains(WidgetState.selected)) {
+              return TextStyle(
+                color: clr.greyBlue,
+                fontWeight: FontWeight.bold,
+                fontSize: 10,
+                fontFamily: "Inter",
+              );
+            }
+
+            return TextStyle(
+              color: clr.matteblack,
+              fontWeight: FontWeight.bold,
+              fontSize: 10,
+              fontFamily: "Inter",
+            );
+          }),
+          labelBehavior:
+              NavigationDestinationLabelBehavior.alwaysShow,
+        ),
+      ),
+      home: const RecordsPage(),
+    ),
+  );
 }
 
 class RecordsPage extends StatefulWidget {
