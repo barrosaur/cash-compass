03bed0e260d93e06cb11c5ce4cdc4e58b5e12254
Ellaine Joyce Barro <mpacc006@gmail.com>
2026-04-22 11:04:36 +0800

    UPDATE: each page defined

---
+++ lib/pages/budget_goal_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BudgetGoalPage extends StatefulWidget {
+  const BudgetGoalPage({super.key});
+
+  @override
+  State<BudgetGoalPage> createState() =>
+      _BudgetGoalPageState();
+}
+
+class _BudgetGoalPageState extends State<BudgetGoalPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
