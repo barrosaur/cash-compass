8316dda5b446f2e4c88411340028563f0a242ea8
Ellaine Joyce Barro <mpacc006@gmail.com>
2026-04-25 12:02:06 +0800

    ADDED: navigation bar for home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
+import 'package:cash_compass/widgets/add_btn.dart';
+import 'package:cash_compass/widgets/expense_record.dart';
 import 'package:flutter/material.dart';
+import 'pages/accounts_page.dart';
+import 'pages/budget_goal_page.dart';
+import 'pages/categories_page.dart';
+import 'pages/transaction_page.dart';
+import 'widgets/appbar.dart';
 import 'constants/colors.dart' as clr;
+import 'constants/nav_destinations.dart';
+import 'classes/transaction.dart';
+import 'package:intl/intl.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(TransactionModeAdapter());
+  Hive.registerAdapter(TransactionAdapter());
+
+  await Hive.openBox<Transaction>('transaction');
+
   runApp(
     MaterialApp(
       theme: ThemeData(
@@ -55,6 +74,47 @@ class RecordsPage extends StatefulWidget {
 }
 
 class _RecordsPageState extends State<RecordsPage> {
+  int _index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> pages = [
+      const RecordsPage(),
+      TransactionPage(),
+      AccountsPage(),
+      CategoriesPage(),
+      BudgetGoalPage(),
+    ];
+
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      body: Padding(
+        padding: const EdgeInsetsGeometry.all(10),
+        child: pages[_index],
+      ),
+      floatingActionButton: [1, 2, 3, 4].contains(_index)
+          ? null
+          : AddBtn(
+              onToggle: () => setState(() => _index = 1),
+            ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.endFloat,
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: clr.matteblack,
+        selectedIndex: _index,
+        labelBehavior:
+            NavigationDestinationLabelBehavior.alwaysShow,
+        onDestinationSelected: (idx) =>
+            setState(() => _index = idx),
+        destinations: navDestinations,
+      ),
+    );
+  }
+}
+
+class RecordsBody extends StatelessWidget {
+  const RecordsBody({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
